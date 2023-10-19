e1d33f2a061c861ea2e6e4311b55d05cd6cbf18b
Fernando Barcala Rodriguez <fbarcalarodrigez@vector-software.com>
2023-10-19 08:36:47 +0200

    add icons and styles to home

--- src/layout/menu-bar/menu-bar.jsx
+++ src/layout/menu-bar/menu-bar.jsx
@@ -1,4 +1,7 @@
 import Add from '@material-ui/icons/Add';
+import Home from '@material-ui/icons/Home';
+import Menu from '@material-ui/icons/Menu';
+import Filter from '@material-ui/icons/FilterList';
 import { useState } from 'react';
 
 const MenuBar = () => {
@@ -7,13 +10,14 @@ const MenuBar = () => {
         <div className='absolute h-fit bottom-0 left-0 right-0 transition-all'>
             <div className="h-1/6 flex justify-between mt-5 bg-light-black">
                 <div>
-                    <button>TODO: buttons left</button>
+                    <button><Home /></button>
+                    <button><Menu /></button>
                 </div>
                 <button className="bg-dark-yellow rounded-full leading-none w-10 h-10 -translate-y-5" onClick={()=>setIsAddMovementPanelActive(prev=>!prev)}>
                             <Add className='text-black'/>
                 </button>
                 <div>
-                    <button>TODO: buttons right</button>
+                    <button><Filter /></button>
                 </div>
             </div>
             <AddMovementContainer isActive={isAddMovementPanelActive}/>
@@ -26,7 +30,7 @@ export default MenuBar;
 const AddMovementContainer = ({isActive}) => {
     return ( 
         <div className={`bg-black transition-all overflow-hidden ${isActive ? 'pt-5 pb-5' : ''} pl-5 pr-5 h-fit`}>
-            <div className={`transition-all ${isActive ? 'h-20' : 'h-0'} w-full text-light-black`}>
+            <div className={`transition-all ${isActive ? 'h-20' : 'h-0'} w-full text-light-black duration-300`}>
                     <textarea className='bg-white w-full h-full pl-2 pr-2 pt-1 pb-1 resize-none rounded-md'></textarea>
             </div>
         </div>
